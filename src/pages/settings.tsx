e05bf787362873936d7d0145f133c1b73f21875d
aomkirby123 <aomkirby123@gmail.com>
2019-03-14 13:44:10 +0700

    Add 'add to homescreen' button on Android device which hasn't add Papership to their homescreen yet

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -144,10 +144,32 @@ export default class extends Component<{},state> {
             for(let registration of registrations) {
                 registration.unregister()
             } 
+        }).then(() => {
+            window.location.reload();
         })
     }
 
+    reload = ():void => {
+        window.location.reload();
+    }
+
+    addToHomescreen = ():void => {
+        let deferredPrompt;
+
+        window.addEventListener('beforeinstallprompt', (e:any) => {
+            e.preventDefault();
+
+            deferredPrompt = e;
+            deferredPrompt.prompt();
+
+            deferredPrompt.userChoice.then((choiceResult:any) => {
+                deferredPrompt = null;
+            });
+        });
+    }
+
     render(){
+        let isAndroid = /(android)/i.test(navigator.userAgent);
         return(
             <>
                 <div id="appbar" style={{filter: `blur(${this.state.blur}px)`}}>
@@ -162,10 +184,25 @@ export default class extends Component<{},state> {
                     <div></div>
                 </div>
                 <div id="settings" style={{filter: `blur(${this.state.blur}px)`}}>
+                    {isAndroid && !window.matchMedia('(display-mode: standalone)').matches ?
+                        <SettingPanel
+                            title="Add to homescreen"
+                            content="Save Papership to homescreen for easier usage with offline support and native feels (Hide URL bar and Navigation bar)."
+                            button="Add to homescreen"
+                            function={() => this.addToHomescreen()}
+                        />
+                    : null
+                    }
                     <SettingPanel
                         title="Storage"
                         content="Estimate storage usage and maximum storage usage"
                         button={`Using ${this.state.usage} out of ${this.state.quota} MB.`}
+                    />
+                    <SettingPanel 
+                        title="Reload"
+                        content="Reload app to from fresh start without reopen app."
+                        button="Reload"
+                        function={() => this.reload()}
                     />
                     <SettingPanel 
                         title="Clear Cache"
